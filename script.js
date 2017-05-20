fbd33c2c44e26280dd05429c9098698200fc26e6
kguo901 <guo.901@osu.edu>
2017-05-20 12:27:48 -0500

    changes to profile

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+var id = ;
+
+function addToCart() {
+    var dropdown = document.getElementById("dropdown");
+    id = dropdown.options[dropdown.selectedIndex].text;
+}
+
 function highlight(event) {
     document.getElementById("active").id = "";
     event.id = "active";
+    alert(id);
+    if (document.getElementById(id).parentElement.id == "active") {
+        var p = document.getElementById("new");
+        var image = document.createElement("img");
+        image.src = "http://target.scene7.com/is/image/Target/51183940?wid=360&hei=360&qlt=80&fmt=pjpeg";
+        p.appendChild(image);
+    }
 }
